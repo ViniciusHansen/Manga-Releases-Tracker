bbfe05af7f74f8b144676fa1a0bd27d21473add9
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2021-06-20 18:50:54 -0300

    Optimize tracker.py

--- tracker.py
+++ tracker.py
@@ -1,13 +1,5 @@
 from selenium import webdriver
 import time
-import os
-
-
-def clearConsole():
-    command = 'clear'
-    if os.name in ('nt', 'dos'):
-        command = 'cls'
-    os.system(command)
 
 
 chrome_options = webdriver.ChromeOptions()
@@ -21,26 +13,24 @@ mangas = {
     'Boku no Hero': 'https://mangalivre.net/manga/boku-no-hero-academia/1319'
 }
 
-clearConsole()
+print('\n')
 print('Buscando Dados...')
-print('\n' * 2)
+print('\n')
 
 for manga in mangas:
     driver.get(mangas[manga])
-    time.sleep(2)
+    time.sleep(1.5)
     data_ultimo_cap = driver.find_element_by_xpath(
         '//*[@id="chapter-list"]/div[2]/ul/li[1]/a/div[1]/span')
     numero_ultimo_cap = driver.find_element_by_xpath(
         '//*[@id="chapter-list"]/div[2]/ul/li[1]/a/div[2]/div/span[1]')
     link = driver.find_element_by_xpath(
         '//*[@id="chapter-list"]/div[2]/ul/li[1]/a').get_attribute("href")
-    titulo = driver.title
 
-    print(titulo)
+    print(driver.title)
     print('-' * 48)
     print('Último capítulo: {}, publicado em: {} \n{}'.format(
         numero_ultimo_cap.text[-3::], data_ultimo_cap.text, link))
     print('\n')
 
-print('\n')
 driver.quit()
