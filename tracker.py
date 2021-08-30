b313f7d56f2db150d21d032436ba38369b7a25e9
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2021-08-30 16:41:23 -0300

    optimize tracker.py

--- tracker.py
+++ tracker.py
@@ -1,16 +1,32 @@
 from selenium import webdriver
-import time
 
 
 def reset_settings():
     chrome_options = webdriver.ChromeOptions()
     # chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--log-level=3')
+    chrome_options.add_experimental_option(
+        'excludeSwitches', ['enable-logging'])
     global driver
     driver = webdriver.Chrome(options=chrome_options)
 
 
-reset_settings()
+def main():
+    print('\nBuscando Dados...\n')
+    reset_settings()
+    for manga in mangas:
+        driver.get(manga)
+        data_ultimo_cap = driver.find_element_by_xpath(
+            '//*[@id="chapter-list"]/div[2]/ul/li[1]/a/div[1]/span')
+        numero_ultimo_cap = driver.find_element_by_xpath(
+            '//*[@id="chapter-list"]/div[2]/ul/li[1]/a/div[2]/div/span[1]')
+        link = driver.find_element_by_xpath(
+            '//*[@id="chapter-list"]/div[2]/ul/li[1]/a').get_attribute("href")
+        print(driver.title[:-27])
+        print(
+            f'Último capítulo: {numero_ultimo_cap.text[-3::]}, publicado em: {data_ultimo_cap.text} \n{link}\n\n')
+        driver.close()
+        reset_settings()
+    driver.quit()
 
 
 mangas = (
@@ -19,23 +35,5 @@ mangas = (
     'https://mangalivre.net/manga/boku-no-hero-academia/1319'
 )
 
-print('\nBuscando Dados...\n')
-
-
-for manga in mangas:
-    driver.get(manga)
-    data_ultimo_cap = driver.find_element_by_xpath(
-        '//*[@id="chapter-list"]/div[2]/ul/li[1]/a/div[1]/span')
-    numero_ultimo_cap = driver.find_element_by_xpath(
-        '//*[@id="chapter-list"]/div[2]/ul/li[1]/a/div[2]/div/span[1]')
-    link = driver.find_element_by_xpath(
-        '//*[@id="chapter-list"]/div[2]/ul/li[1]/a').get_attribute("href")
-
-    print('-' * 48)
-    print(driver.title[:-27])
-    print('Último capítulo: {}, publicado em: {} \n{}\n\n'.format(
-        numero_ultimo_cap.text[-3::], data_ultimo_cap.text, link))
-    driver.close()
-    reset_settings()
-    # time.sleep(1.5)
-driver.quit()
+if __name__ == '__main__':
+    main()
